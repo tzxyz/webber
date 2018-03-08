8a12a930e8dd2f9e67fce3d71c732b3e647e06d7
zhuonima <tz.zhuo@protonmail.com>
2018-03-08 16:06:30 +0800

    添加scheduler接口 & 实现QueueScheduler

--- scheduler.go
+++ scheduler.go
@@ -1,1 +1,37 @@
 package webber
+
+import (
+	"sync"
+	"container/list"
+	log "github.com/sirupsen/logrus"
+)
+
+type Scheduler interface {
+	Push(request *Request)
+	Poll() *Request
+}
+
+type QueueScheduler struct {
+	queue *list.List
+	lock  sync.Mutex
+}
+
+func (s *QueueScheduler) Push(request *Request) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.queue.PushBack(request)
+	log.Debug("QueueScheduler Push Url: ", request.url)
+}
+
+func (s *QueueScheduler) Poll() *Request {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	e := s.queue.Front()
+	if e == nil {
+		return nil
+	}
+	s.queue.Remove(e)
+	request := e.Value.(*Request)
+	log.Debug("QueueScheduler Poll Url: ", request.url)
+	return request
+}
