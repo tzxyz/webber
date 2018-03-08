9c8f70a8dc8b72f13359963e845675ee49c5776b
zhuonima <tz.zhuo@protonmail.com>
2018-03-08 18:20:59 +0800

    clean up & 添加meizitu例子

--- examples/meizitu.go
+++ examples/meizitu.go
@@ -1,1 +1,23 @@
-package examples
+package main
+
+import (
+	"strings"
+	"github.com/zhuonima/webber"
+)
+
+func main() {
+	webber.New().
+		Name("妹子图").
+		StartUrls("http://www.meizitu.com/a/more_1.html").
+		Processor(func(response *webber.Response) *webber.Result {
+		// 列表页
+		if strings.HasPrefix(response.GetUrl(), "http://www.meizitu.com/a/more_") {
+			links := response.Html().Xpath("//h3[@class = 'tit']/a/@href")
+			return webber.NewResult().PushUrls(links...)
+		}
+		// 详情页
+		return webber.NewResult().
+			PushItem("images", response.Html().Xpath("//div[@id='picture']/p/img/@src")).
+			PushItem("title", response.Html().Xpath("//div[@class='metaRight']/h2/a/text()"))
+	}).Start()
+}
--- webber.go
+++ webber.go
@@ -1,5 +1,11 @@
 package webber
 
+import (
+	"sync"
+	"time"
+	log "github.com/sirupsen/logrus"
+)
+
 type Webber struct {
 	name       string
 	startUrls  []string
@@ -20,32 +26,83 @@ func New() *Webber {
 	}
 }
 
-func (engine *Webber) Name(name string) *Webber {
-	engine.name = name
-	return engine
+func (w *Webber) Name(name string) *Webber {
+	w.name = name
+	return w
 }
 
-func (engine *Webber) StartUrls(urls ...string) *Webber {
-	engine.startUrls = urls
-	return engine
+func (w *Webber) StartUrls(urls ...string) *Webber {
+	w.startUrls = urls
+	return w
 }
 
-func (engine *Webber) Downloader(downloader Downloader) *Webber {
-	engine.downloader = downloader
-	return engine
+func (w *Webber) Downloader(downloader Downloader) *Webber {
+	w.downloader = downloader
+	return w
 }
 
-func (engine *Webber) Scheduler(scheduler Scheduler) *Webber {
-	engine.scheduler = scheduler
-	return engine
+func (w *Webber) Scheduler(scheduler Scheduler) *Webber {
+	w.scheduler = scheduler
+	return w
 }
 
-func (engine *Webber) Processor(processor Processor) *Webber {
-	engine.processor = processor
-	return engine
+func (w *Webber) Processor(processor Processor) *Webber {
+	w.processor = processor
+	return w
 }
 
-func (engine *Webber) Pipelines(pipelines ...Pipeline) *Webber {
-	engine.pipelines = pipelines
-	return engine
+func (w *Webber) Pipelines(pipelines ...Pipeline) *Webber {
+	w.pipelines = pipelines
+	return w
+}
+
+func (w *Webber) Start() {
+
+	for _, url := range w.startUrls {
+		w.scheduler.Push(NewRequest().Url(url))
+	}
+
+	var wg sync.WaitGroup
+
+	for {
+
+		req := w.scheduler.Poll()
+
+		if req == nil && len(w.running) == 0 {
+			break
+		} else if req == nil {
+			time.Sleep(time.Second)
+		} else {
+			wg.Add(1)
+			go func() {
+
+				w.running <- req
+
+				defer func() {
+					<-w.running
+					wg.Done()
+				}()
+
+				resp := w.downloader(req)
+
+				log.Info("Downloader download Url: ", req.url)
+
+				result := w.processor(resp)
+
+				log.Info("Processor process Url: ", req.url)
+
+				for _, pipeline := range w.pipelines {
+					pipeline(result)
+				}
+
+				if result.HasNextUrl() {
+
+					for _, url := range result.NextUrls() {
+						w.scheduler.Push(NewRequest().Url(url))
+					}
+				}
+			}()
+			wg.Wait()
+		}
+	}
 }
