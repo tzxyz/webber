8b9cee9941ac311b0f6f6ef75c4114e42b1566b6
zhuotianzhu <tz.zhuo@protonmail.com>
2019-06-25 18:55:52 +0800

    :arrow_up: add go mod support

--- examples/meizitu.go
+++ examples/meizitu.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/tzxyz/webber"
 	"strings"
-	"github.com/zhuonima/webber"
 )
 
 func main() {
@@ -10,14 +10,14 @@ func main() {
 		Name("妹子图").
 		StartUrls("http://www.meizitu.com/a/more_1.html").
 		Processor(func(response *webber.Response) *webber.Result {
-		// 列表页
-		if strings.HasPrefix(response.Url(), "http://www.meizitu.com/a/more_") {
-			links := response.Html().Xpath("//h3[@class = 'tit']/a/@href")
-			return webber.NewResult().PushUrls(links...)
-		}
-		// 详情页
-		return webber.NewResult().
-			PushItem("images", response.Html().Xpath("//div[@id='picture']/p/img/@src")).
-			PushItem("title", response.Html().Xpath("//div[@class='metaRight']/h2/a/text()"))
-	}).Start()
+			// 列表页
+			if strings.HasPrefix(response.Url(), "http://www.meizitu.com/a/more_") {
+				links := response.Html().Xpath("//h3[@class = 'tit']/a/@href")
+				return webber.NewResult().PushUrls(links...)
+			}
+			// 详情页
+			return webber.NewResult().
+				PushItem("images", response.Html().Xpath("//div[@id='picture']/p/img/@src")).
+				PushItem("title", response.Html().Xpath("//div[@class='metaRight']/h2/a/text()"))
+		}).Start()
 }
---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/tzxyz/webber
+
+go 1.12
+
+require (
+	github.com/elazarl/goproxy v0.0.0-20190421051319-9d40249d3c2f // indirect
+	github.com/elazarl/goproxy/ext v0.0.0-20190421051319-9d40249d3c2f // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+	github.com/parnurzeal/gorequest v0.2.15
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/sirupsen/logrus v1.0.5
+	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190618222545-ea8f1a30c443 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	golang.org/x/sys v0.0.0-20190621062556-bf70e4678053 // indirect
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+	gopkg.in/xmlpath.v2 v2.0.0-20150820204837-860cbeca3ebc
+)
